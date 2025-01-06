d9d6818f0c9491884f139745d00cb8cd03a5ea88
Arslanamin404 <arslanamin.org@gmail.com>
2025-01-06 12:16:27 +0530

    Minor bug fixe

--- src/middlewares/ErrorHandler.ts
+++ src/middlewares/ErrorHandler.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from 'express';
-import { API_Response } from '../utils/ApiResponse';
 
 // Define the error type explicitly as a general Error
 interface CustomError extends Error {
@@ -7,14 +6,13 @@ interface CustomError extends Error {
     message: string;
 }
 
-export const ErrorHandler = (
-    err: CustomError,
-    req: Request,
-    res: Response,
-    next: NextFunction
-): Response => {
+export const ErrorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction): void => {
     const errStatus = err.statusCode || 500;
     const errMsg = err.message || 'Something went wrong';
 
-    return API_Response(res, errStatus, false, errMsg)
+    res.status(errStatus).json({
+        success: false,
+        status: errStatus,
+        message: errMsg,
+    });
 };
